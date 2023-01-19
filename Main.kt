5c81fa4ebeb883304478605fa0bb9e224a744cb3
Kirill Lemesev= <boyanya@gmail.com>
2023-01-19 23:24:07 +0600

    second stage implementation

--- Main.kt
+++ Main.kt
@@ -1,5 +1,30 @@
 package encryptdecrypt
 
 fun main() {
-    println("Hello World!")
+
+    val alphabet = "abcdefghijklmnopqrstuvwxyz"
+    val phrase = readln()
+    val key = readln().toByte()
+
+    for (ch in phrase) {
+
+        if (ch.isWhitespace()) {
+            print(" ")
+        }
+        else {
+
+            val index = alphabet.indexOf(ch)
+            var shift = index + key
+            if (shift > alphabet.length - 1) {
+                shift = shift - alphabet.length
+                print(alphabet[shift])
+            }
+            else {
+                print(alphabet[shift])
+            }
+        }
+
+    }
+
+
 }

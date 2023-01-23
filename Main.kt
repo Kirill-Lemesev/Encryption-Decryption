1aae850f28ca11902794b6d8ab8618a81f8596e0
Kirill Lemesev= <boyanya@gmail.com>
2023-01-23 23:32:44 +0600

    fourth stage

--- Main.kt
+++ Main.kt
@@ -1,19 +1,17 @@
 package encryptdecrypt
 
-fun main() {
+fun main(args: Array<String>) {
 
-    val function = readln() //dec or enc
-    val phrase = readln()
-    val key = readln().toInt()
+    val function = if (args.indexOf("-mode") != null) args[args.indexOf("-mode") + 1] else "enc"
+    val phrase = if (args.indexOf("-data") != null) args[args.indexOf("-dataф") + 1] else ""
+    val key  = if (args.indexOf("-key") != null) args[args.indexOf("-key") + 1].toInt() else 0
 
     for (ch in phrase) {
 
         if (function == "enc") {
             print(ch.toChar() + key)
-        } else
-        {
-            print(ch.toChar() - key)
+        } else {
+           print(ch.toChar() - key)
         }
     }
-
 }

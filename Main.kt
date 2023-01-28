70837452466a152f43ea9f3b0728fabb3d571a0a
Kirill Lemesev= <boyanya@gmail.com>
2023-01-28 22:07:20 +0600

    second version fifth stage

--- Main.kt
+++ Main.kt
@@ -7,16 +7,18 @@ fun main(args: Array<String>) {
     val mode = if (args.indexOf("-mode") != null) args[args.indexOf("-mode") + 1] else "enc"
     val data = if (args.indexOf("-data") != null) args[args.indexOf("-data") + 1] else ""
     val key = if (args.indexOf("-key") != null) args[args.indexOf("-key") + 1].toInt() else 0
-    val input = if (args.indexOf("-in") != null) args[args.indexOf("-in") + 1] else ""
+    val input =  if (args.indexOf("-in") != null) args[args.indexOf("-in") + 1] else ""
     val output = if (args.indexOf("-out") != null) args[args.indexOf("-out") + 1] else ""
     var buffer: String
 
     fun changeString(data: String, mode: String, key: Int): String {
-        return if (mode == "end") {
-            data.map { it.toChar() + key }.toString()
-        } else {
-            data.map { it.toChar() - key }.toString()
+
+        var temp: String
+
+        for (ch in data) {
+            temp += if (mode == "enc") ch.toChar() + key else ch.toChar() - key
         }
+        return  temp
     }
 
 
@@ -29,14 +31,10 @@ fun main(args: Array<String>) {
         ""
     }
 
-
     if (output != "") {
         val outputFile = File(output).writeText(buffer)
     } else {
         print(buffer)
     }
-
-
-
 
 }
